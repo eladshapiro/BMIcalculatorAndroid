339b8feb649b075dacc4a1877e61c111f2333c1f
Elad shapiro <eladshapiro@gmail.com>
2022-05-31 18:29:06 +0300

    all the places are done, now we need to make the submit and clear

--- app/src/main/java/com/example/bmicalculatorandroid/MainActivity.java
+++ app/src/main/java/com/example/bmicalculatorandroid/MainActivity.java
@@ -4,19 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText firstNameEditText;
     EditText lastNameEditText;
     EditText ageEditText;
+    SeekBar heightSeekBar;
+    TextView selectedHeightTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //firstName,lastName,Age
         firstNameEditText=findViewById(R.id.FirstNameEditText);
         lastNameEditText=findViewById(R.id.LastNameEditText);
         ageEditText=findViewById(R.id.ageEditTextNumber);
+
+        //height seek bar
+        heightSeekBar = (SeekBar) findViewById(R.id.HightSeekBar);
+        selectedHeightTextView = (TextView) findViewById(R.id.selectedHightTextView);
+
+        heightSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                selectedHeightTextView.setText("selected height = " + heightSeekBar.getProgress()+" cm");
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
